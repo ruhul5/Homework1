f27e48e63048afb953b3b547868d071813511d85
Ruhul Alam <ruhul51218@gmail.com>
2018-04-15 10:56:42 -0400

    new code

---
+++ src/newhomework1/new1.java
@@ -0,0 +1,27 @@
+package newhomework1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class new1 {
+
+	
+	@Test 
+	public void testingFirstName() {
+
+
+
+		WebDriver driver = new ChromeDriver();
+
+		System.setProperty("webdriver.chrome.driver","//Users//ruhulalam//framework2018//drivers//chromedriver");
+		driver.get ("http://www.facebook.com");
+
+		//new test	
+	
+	
+	
+	
+	
+	
+}}
